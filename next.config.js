73905e7126dc8623f2c9e5b425e9cf79d4ff7bd1
shamimbinnur <sbnsbn00@gmail.com>
2022-03-21 23:36:46 +0600

    ignored eslint

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false
+  swcMinify: false,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 
 
 }
